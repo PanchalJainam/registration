d2902fc08bd07cbcbf468c5e85d2bd1e30369f81
jainam panchal <jainampancha345@gmail.com>
2022-07-13 16:57:45 +0530

    Registration Form

---
+++ src/db/conn.js
@@ -0,0 +1,5 @@
+const mongoose=require('mongoose')
+
+mongoose.connect("mongodb://localhost:27017/registerdemo")
+.then(()=>{console.log("connection successfully")})
+.catch((e)=>console.log(e));
---
+++ src/models/app.js
@@ -0,0 +1,68 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const hbs = require('hbs')
+const PORT=process.env.PORT || 3000;
+
+require('../db/conn')
+const Register=require('./register')
+const publicPath = path.join(__dirname, '../../public')
+const templatePath = path.join(__dirname, '../../templates/views');
+const partialPath = path.join(__dirname, '../../templates/partials');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+app.use(express.static(publicPath))
+
+app.set('view engine','hbs');
+
+app.set('views',templatePath);
+
+hbs.registerPartials(partialPath);
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+app.get('/about', (req, res) => {
+    res.render('about',{
+        title:"about page"
+    });
+});
+
+app.get('/registration', (req, res) => {
+    res.render('registration');
+});
+
+app.post('/registration', async (req, res) => {
+    try{
+        const password=req.body.password;
+        const cpassword=req.body.cpassword;
+
+        if(password==cpassword)
+        {
+            const registerUser=new Register({
+                Firstname:req.body.firstname,
+                Lastname:req.body.lastname,
+                Email:req.body.email,
+                Password:password,
+                ConfirmPassword:cpassword
+            })
+
+            const registerResult= await registerUser.save();
+            res.status(201).render("index");
+        }
+        else{
+            res.send("password is not matching");
+        }
+    }
+    catch(err){
+        res.status(400).send(err);
+    }
+});
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running on ${PORT}`)
+})
